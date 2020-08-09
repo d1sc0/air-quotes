1ef74557a9d5a39140c08b7b606bcaf144426529
d1sc0 <ilovediscostu@gmail.com>
2020-08-09 19:02:34 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,52 @@
+// set varaibles for relevant DOM items
+const quoteContainer = document.getElementById('container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newquoteBtn = document.getElementById('new-quote');
+
+// Get quote from Api
+async function getQuote() {
+  const apiUrl = 'https://sjm-api-proxy.herokuapp.com/api/airquote/random';
+
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+
+    // check and replace blank author values
+    if (data.fields.quoteAuthor === '') {
+      authorText.innerText = 'Unknown';
+    } else {
+      authorText.innerText = data.fields.quoteAuthor;
+    }
+
+    // change font size based on quote length
+    if (data.fields.quoteText.length > 110) {
+      quoteText.classList.add('long-quote');
+    } else {
+      quoteText.classList.remove('long-quote');
+    }
+
+    // populate data
+    authorText.innerText = data.fields.quoteAuthor;
+    quoteText.innerText = data.fields.quoteText;
+    console.log(data);
+  } catch (error) {
+    console.log('whooops, no quote!', error);
+  }
+}
+
+// tweet quote function
+function tweetQuote() {
+  const quote = quoteText.innerText;
+  const author = authorText.innerText;
+  const twitterUrl = `https://twitter.com/intent/tweet/?text=${quote} - ${author}`;
+  window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+newquoteBtn.addEventListener('click', getQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+// On Load
+getQuote();
